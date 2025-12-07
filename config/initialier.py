6a8b8d44719f6ebf718e856e0d81267149539ced
Gnatear <tzh26144@gmail.com>
2025-12-07 13:05:08 -0400

    added default settings of city, days, coordinations. API Keys need to be added later

---
+++ config/initialier.py
@@ -0,0 +1,8 @@
+DEFAULT_CITY = "Halifax"
+DEFAULT_DAYS = 7 #time frame
+DEFAULT_LAT = 44.6488 #coordination of Halifax
+DEFAULT_LON = -63.5752 #coordination of Halifax
+
+API_KEYS = {
+    "weatherapi": None,
+}
